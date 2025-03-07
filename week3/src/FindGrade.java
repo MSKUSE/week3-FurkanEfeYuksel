c86fc07b2a0526ed2bdd50dd62692f6809e2d972
U220717012 <furkanefeyuksel@posta.mu.edu.tr>
2025-03-07 11:57:30 +0300

    weeek3

--- week3/src/FindGrade.java
+++ week3/src/FindGrade.java
@@ -1,6 +1,6 @@
-public class FindGrade{
+public class FindGrade {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //Get the score from the user as a file parameter
         //Calculate the grade based on the score
@@ -10,13 +10,32 @@ public class FindGrade{
         // 70 - 79 -> C
         // 60 - 69 -> D
         // 0 - 59 -> F
+        if (args.length >= 1) {
+            int score = Integer.parseInt(args[0]);
 
 
+            if (score >= 90 && score<=100) {
+                System.out.println("Your grade is A");
 
+            } else if (score >= 80) {
+                System.out.println("Your grade is B");
+
+            } else if (score >= 70) {
+                System.out.println("Your grade is C");
+
+            } else if (score >= 60) {
+                System.out.println("Your grade is D");
+
+            } else if (score > 0) {
+                System.out.println("Your grade is F");
+            } else {
+                System.out.println("invalid value");
+            }
+
+        }
 
 
     }
-
 
 
 }
---
+++ week3/src/FindMinimum.java
@@ -0,0 +1,12 @@
+public class FindMinimum {
+    public static void main(String[] args) {
+        int number1= Integer.parseInt(args[0]);
+        int number2= Integer.parseInt(args[1]);
+        int number3= Integer.parseInt(args[2]);
+
+        boolean num1_min= number1<number2 && number1<number3;
+        boolean num2_min= number2<number1 && number2<number3;
+        int minimum= num1_min ? number1 : num2_min ? number2: number3;
+
+    }
+}
